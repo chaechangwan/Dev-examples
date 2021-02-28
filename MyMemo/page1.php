92115a100227f41be16a7bd44df2197f358a5044
sju-changwan <chang940406@naver.com>
2021-02-28 21:11:23 +0900

    210228(1)

--- MyMemo/page1.php
+++ MyMemo/page1.php
@@ -47,8 +47,14 @@
 <?php
     while($row = mysqli_fetch_array($result)){
 ?>
-                <li>
-                    <a href="#"><?=$row['memo_content']?><a>
+                <li>     
+                    <div style="position:relative;text-align:right;border-bottom:1px solid black;">
+                        <img src="./photo/heart.png" width="20px" style="position: absolute;left:10px;">2021-02-28
+                    </div>
+                    <div>
+                        <a href="#"><?=$row['memo_content']?><a>
+                    </div>               
+            
                     <ul class="dropdown">
                         <li><a href='#'><img src='./photo/heart.png' width="40px" height="40px"></a></li>
                         <li><a href='#'><img src='./photo/trash.png' width="40px" height="40px"></a></li>
